4ac79bf7406f49879c3b48ec0ad81c8b93176be7
League Student <league@iMac-17.attlocal.net>
2020-02-02 11:28:16 -0800

    yur
    ryurtyu

---
+++ src/_01_introduction/CustomException.java
@@ -0,0 +1,12 @@
+package _01_introduction;
+
+public class CustomException extends Exception{
+
+	
+	
+	
+static void terminate() {
+	System.exit(0);
+	
+}
+}
--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,12 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+
+	double divide(double x, double y) {
+		double q = x / y;
+		if (y == 0) {
+		
+		}
+		return q;
+	}
 }
